0453ab3470b866a9023199475b365f5e2bbaabe9
Sarah Lee <sarahjayeunlee@gmail.com>
2019-06-27 15:04:14 -0400

    copied from other

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# lee-sarah.github.io
+Sarah Lee's Personal Website
+
+To Do List 
+- convert px to em
+- add alt=" " to all images
---
+++ javascript/scroll.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+document.getElementById("scrollTopbtn").style.display = "block";
+} else {
+document.getElementById("scrollTopbtn").style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
